a451e334415fcab4a83ca2efd49a4ba19bfb7a89
Avadhutjc <Avadhutjc@gmail.com>
2021-12-17 20:37:24 +0530

    done

--- app/src/main/java/com/ajc/search_app/repository/LocationRepository.kt
+++ app/src/main/java/com/ajc/search_app/repository/LocationRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import com.ajc.search_app.models.remote.ResponseDTO
 import com.masai.searchapp.models.remote.api.ApiService
 
+
 class LocationRepository(private val userApi: ApiService) {
     private val contentType = "application/json"
     private val userLiveData = MutableLiveData<ResponseDTO>()
